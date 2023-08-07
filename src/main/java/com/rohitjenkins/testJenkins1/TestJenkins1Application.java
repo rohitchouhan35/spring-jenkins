0e15e1278a33d20a2269fac73dc0663c13692405
rohit <rohitchouhan3512@gmail.com>
2023-08-08 00:17:22 +0530

    initial

---
+++ src/main/java/com/rohitjenkins/testJenkins1/TestJenkins1Application.java
@@ -0,0 +1,13 @@
+package com.rohitjenkins.testJenkins1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkins1Application.class, args);
+	}
+
+}
